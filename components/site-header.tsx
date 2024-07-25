e2ebdf37af708cf5b4fbe588dae1fd5bc7861446
DarkInventor <ktmehta25@gmail.com>
2024-07-25 03:17:36 -0400

    change sidebar with floating nav

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -5,13 +5,15 @@ import { buttonVariants } from "@/components/ui/button"
 import { Icons } from "@/components/icons"
 import { MainNav } from "@/components/main-nav"
 import { ThemeToggle } from "@/components/theme-toggle"
+import { DockDemo } from "./dock-demo"
 
 export function SiteHeader() {
   return (
-    <header className="bg-background sticky top-0 z-40 w-full border-b">
+    <header className="bg-background sticky top-0 z-40 w-full">
       <div className="container flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
-        <MainNav items={siteConfig.mainNav} />
-        <div className="flex flex-1 items-center justify-end space-x-4">
+        {/* <MainNav items={siteConfig.mainNav} /> */}
+        <DockDemo />
+        {/* <div className="flex flex-1 items-center justify-end space-x-4">
           <nav className="flex items-center space-x-1">
             <Link
               href={siteConfig.links.github}
@@ -45,7 +47,7 @@ export function SiteHeader() {
             </Link>
             <ThemeToggle />
           </nav>
-        </div>
+        </div> */}
       </div>
     </header>
   )
